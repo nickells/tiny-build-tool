6cf19f7ceeaa0b09607d919d967af3ae2b9bd9c2
nickells <ellsworth.nick@gmail.com>
2016-07-18 22:29:44 -0400

    made better

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
-const makeTemplate = require('./module.js')
+'use strict';
 
-let options = {
-  templatePath: './template.html',
-  documentPath: './test.html',
-  renderTarget: '#blog',
-  contentPath: './content.json',
+const Interpolation = require('tiny-interpolator')
+const cheerio = require('cheerio')
+const fs = require('fs')
+const bluebird = require('bluebird')
+const readFile = bluebird.promisify(fs.readFile)
+
+
+function writeHTML(filePath, template, content, target, documentPath){
+  let parser = cheerio.load(documentPath)
+  let elem = parser(target)
+  elem.html(new Interpolation(content,template).output);  
+  return fs.writeFile(filePath, parser.html())
 }
 
-makeTemplate({
-  templatePath: './template.html',
-  documentPath: './test.html',
-  renderTarget: '#blog',
-  contentPath: './content.json',
-})
+function makeFile(options){
+  return bluebird.props({
+    template: readFile(options.templatePath,'utf8'),
+    content: readFile(options.contentPath, 'utf8'),
+    documentPath: readFile(options.documentPath, 'utf8'),
+    outputPath: options.outputPath ? options.outputPath : options.templatePath
+  })
+  .then(data=>{
+    const { template, content, documentPath, outputPath } = data
+    return writeHTML(outputPath, template, JSON.parse(content), options.renderTarget, documentPath)
+  })
+  .catch(err=>{
+    console.log(err)
+  })
+}
+
+module.exports = makeFile;
